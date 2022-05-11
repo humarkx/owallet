c0408b3db45a729e583370ae32cba1d92f1486b1
Pham Tu <tubackkhoa@gmail.com>
2022-05-11 11:01:54 +0700

    initial commit

--- packages/stores/src/query/cosmos/governance/proposals.ts
+++ packages/stores/src/query/cosmos/governance/proposals.ts
@@ -1,18 +1,18 @@
-import { ObservableChainQuery } from "../../chain-query";
-import { GovProposals } from "./types";
-import { computed, makeObservable, observable, runInAction } from "mobx";
+import { ObservableChainQuery } from '../../chain-query';
+import { GovProposals } from './types';
+import { computed, makeObservable, observable, runInAction } from 'mobx';
 import {
   ObservableQueryGovParamDeposit,
   ObservableQueryGovParamTally,
-  ObservableQueryGovParamVoting,
-} from "./params";
-import { KVStore } from "@keplr-wallet/common";
-import { ChainGetter } from "../../../common";
-import { StakingPool } from "../staking/types";
-import { DeepReadonly } from "utility-types";
-import { Dec, DecUtils, Int, IntPretty } from "@keplr-wallet/unit";
-import { computedFn } from "mobx-utils";
-import { ObservableQueryProposal } from "./proposal";
+  ObservableQueryGovParamVoting
+} from './params';
+import { KVStore } from '@keplr-wallet/common';
+import { ChainGetter } from '../../../common';
+import { StakingPool } from '../staking/types';
+import { DeepReadonly } from 'utility-types';
+import { Dec, DecUtils, Int, IntPretty } from '@keplr-wallet/unit';
+import { computedFn } from 'mobx-utils';
+import { ObservableQueryProposal } from './proposal';
 
 export class ObservableQueryGovernance extends ObservableChainQuery<GovProposals> {
   @observable.ref
@@ -28,7 +28,7 @@ export class ObservableQueryGovernance extends ObservableChainQuery<GovProposals
     chainGetter: ChainGetter,
     protected readonly _queryPool: ObservableChainQuery<StakingPool>
   ) {
-    super(kvStore, chainId, chainGetter, "/gov/proposals?limit=1000");
+    super(kvStore, chainId, chainGetter, '/gov/proposals?limit=1000');
     makeObservable(this);
   }
 
@@ -107,8 +107,7 @@ export class ObservableQueryGovernance extends ObservableChainQuery<GovProposals
     // https://github.com/chainapsis/keplr-wallet/issues/275
     // https://github.com/chainapsis/keplr-wallet/issues/278
     // TODO: Erase this part soon
-    console.log("proposals response", this.response);
-    console.log("proposals response data", this.response.data);
+    // console.log("proposals response data", this.response.data);
 
     const result: ObservableQueryProposal[] = [];
 
