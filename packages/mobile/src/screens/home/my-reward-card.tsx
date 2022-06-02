f0f9d56b581fb6433fe3a4a5a337b4e35004de64
sonlha <lhas1307@gmail.com>
2022-06-02 10:09:32 +0700

    setting page

--- packages/mobile/src/screens/home/my-reward-card.tsx
+++ packages/mobile/src/screens/home/my-reward-card.tsx
@@ -65,7 +65,7 @@ export const MyRewardCard: FunctionComponent<{
           <RewardIcon size={44} color={style.get("color-secondary").color} />
         }
         buttonText="Claim"
-        buttonMode="light"
+        buttonMode="outline"
         buttonContainerStyle={style.flatten(["min-width-72"])}
         buttonDisabled={
           !account.isReadyToSendMsgs ||
