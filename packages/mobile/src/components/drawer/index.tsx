a1e1d9a0206f3879462405f5b76d29c1d81255d3
sonlha <lhas1307@gmail.com>
2022-06-20 11:04:09 +0700

    make the drawer looks more smooth

--- packages/mobile/src/components/drawer/index.tsx
+++ packages/mobile/src/components/drawer/index.tsx
@@ -1,20 +1,20 @@
-import React, { FunctionComponent, useCallback } from "react";
-import { observer } from "mobx-react-lite";
+import React, { FunctionComponent, useCallback, useState } from 'react';
+import { observer } from 'mobx-react-lite';
 import {
   DrawerContentComponentProps,
   DrawerContentOptions,
   DrawerContentScrollView,
-} from "@react-navigation/drawer";
-import { useStore } from "../../stores";
-import { DrawerActions, useNavigation } from "@react-navigation/native";
-import { Alert, Text, View } from "react-native";
-import { useStyle } from "../../styles";
-import { RectButton } from "../rect-button";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { VectorCharacter } from "../vector-character";
-import FastImage from "react-native-fast-image";
-import { Hash } from "@owallet/crypto";
-import { BrowserIcon } from "../icon/browser";
+} from '@react-navigation/drawer';
+import { useStore } from '../../stores';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
+import { Alert, Text, View } from 'react-native';
+import { useStyle } from '../../styles';
+import { RectButton } from '../rect-button';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { VectorCharacter } from '../vector-character';
+import FastImage from 'react-native-fast-image';
+import { Hash } from '@owallet/crypto';
+import { BrowserIcon } from '../icon/browser';
 
 export type DrawerContentProps =
   DrawerContentComponentProps<DrawerContentOptions>;
@@ -23,9 +23,7 @@ export const DrawerContent: FunctionComponent<DrawerContentProps> = observer(
   (props) => {
     const { chainStore, analyticsStore } = useStore();
     const navigation = useNavigation();
-
     const safeAreaInsets = useSafeAreaInsets();
-
     const style = useStyle();
 
     const deterministicNumber = useCallback((chainInfo) => {
@@ -40,15 +38,18 @@ export const DrawerContent: FunctionComponent<DrawerContentProps> = observer(
     const profileColor = useCallback(
       (chainInfo) => {
         const colors = [
-          "sky-blue",
-          "mint",
-          "blue-violet",
-          "green",
-          "yellow-green",
-          "purple",
-          "red",
-          "orange",
-          "yellow",
+          'sky-blue',
+          'mint',
+          'red',
+          'orange',
+          'blue-violet',
+          'green',
+          'yellow',
+          'yellow-green',
+          'purple',
+          'red',
+          'orange',
+          'yellow',
         ];
 
         return colors[deterministicNumber(chainInfo) % colors.length];
@@ -63,129 +64,136 @@ export const DrawerContent: FunctionComponent<DrawerContentProps> = observer(
             marginBottom: safeAreaInsets.bottom,
           }}
         >
-          <View style={style.flatten(["justify-center", "height-50"])}>
+          <View style={style.flatten(['justify-center', 'height-50'])}>
             <Text
               style={style.flatten([
-                "h3",
-                "color-text-black-high",
-                "margin-left-24",
+                'h3',
+                'color-text-black-high',
+                'margin-left-24',
               ])}
             >
               Networks
             </Text>
           </View>
-          {chainStore.chainInfosInUI.map((chainInfo) => {
-            const selected = chainStore.current.chainId === chainInfo.chainId;
+          <View>
+            <>
+              {chainStore.chainInfosInUI.map((chainInfo) => {
+                const selected =
+                  chainStore.current.chainId === chainInfo.chainId;
 
-            return (
+                return (
+                  <RectButton
+                    key={chainInfo.chainId}
+                    onPress={() => {
+                      if (!chainInfo.chainName.includes('soon')) {
+                        navigation.dispatch(DrawerActions.closeDrawer());
+                        analyticsStore.logEvent('Chain changed', {
+                          chainId: chainStore.current.chainId,
+                          chainName: chainStore.current.chainName,
+                          toChainId: chainInfo.chainId,
+                          toChainName: chainInfo.chainName,
+                        });
+                        chainStore.selectChain(chainInfo.chainId);
+                        chainStore.saveLastViewChainId();
+                      } else {
+                        Alert.alert('Coming soon!');
+                      }
+                    }}
+                    style={style.flatten([
+                      'flex-row',
+                      'height-84',
+                      'items-center',
+                      'padding-x-20',
+                    ])}
+                    activeOpacity={1}
+                    underlayColor={
+                      style.get('color-drawer-rect-button-underlay').color
+                    }
+                  >
+                    <View
+                      style={style.flatten(
+                        [
+                          'width-32',
+                          'height-32',
+                          'border-radius-64',
+                          'items-center',
+                          'justify-center',
+                          'overflow-hidden',
+                          'margin-right-16',
+                          `background-color-profile-${profileColor(
+                            chainInfo
+                          )}` as any,
+                        ],
+                        [selected && 'background-color-black']
+                      )}
+                    >
+                      {chainInfo.raw.chainSymbolImageUrl ? (
+                        <FastImage
+                          style={{
+                            width: 24,
+                            height: 24,
+                          }}
+                          resizeMode={FastImage.resizeMode.contain}
+                          source={{
+                            uri: chainInfo.raw.chainSymbolImageUrl,
+                          }}
+                        />
+                      ) : (
+                        <VectorCharacter
+                          char={chainInfo.chainName[0]}
+                          height={15}
+                          color="white"
+                        />
+                      )}
+                    </View>
+                    <Text
+                      style={style.flatten(
+                        ['h5', 'color-text-black-very-very-low'],
+                        [selected && 'color-text-black-medium']
+                      )}
+                    >
+                      {chainInfo.chainName}
+                    </Text>
+                  </RectButton>
+                );
+              })}
               <RectButton
-                key={chainInfo.chainId}
+                key={'browser'}
                 onPress={() => {
-                  if (!chainInfo.chainName.includes("soon")) {
-                    analyticsStore.logEvent("Chain changed", {
-                      chainId: chainStore.current.chainId,
-                      chainName: chainStore.current.chainName,
-                      toChainId: chainInfo.chainId,
-                      toChainName: chainInfo.chainName,
-                    });
-                    chainStore.selectChain(chainInfo.chainId);
-                    chainStore.saveLastViewChainId();
-                    navigation.dispatch(DrawerActions.closeDrawer());
-                  } else {
-                    Alert.alert("Coming soon!");
-                  }
+                  navigation.navigate('Browser');
                 }}
                 style={style.flatten([
-                  "flex-row",
-                  "height-84",
-                  "items-center",
-                  "padding-x-20",
+                  'flex-row',
+                  'height-84',
+                  'items-center',
+                  'padding-x-20',
+                  'border-width-top-1',
                 ])}
                 activeOpacity={1}
                 underlayColor={
-                  style.get("color-drawer-rect-button-underlay").color
+                  style.get('color-drawer-rect-button-underlay').color
                 }
               >
                 <View
-                  style={style.flatten(
-                    [
-                      "width-32",
-                      "height-32",
-                      "border-radius-64",
-                      "items-center",
-                      "justify-center",
-                      "overflow-hidden",
-                      "margin-right-16",
-                      `background-color-profile-${profileColor(
-                        chainInfo
-                      )}` as any,
-                    ],
-                    [selected && "background-color-black"]
-                  )}
+                  style={style.flatten([
+                    'width-32',
+                    'height-32',
+                    'border-radius-64',
+                    'items-center',
+                    'justify-center',
+                    'overflow-hidden',
+                    'margin-right-16',
+                    'background-color-profile-green',
+                  ])}
                 >
-                  {chainInfo.raw.chainSymbolImageUrl ? (
-                    <FastImage
-                      style={{
-                        width: 24,
-                        height: 24,
-                      }}
-                      resizeMode={FastImage.resizeMode.contain}
-                      source={{
-                        uri: chainInfo.raw.chainSymbolImageUrl,
-                      }}
-                    />
-                  ) : (
-                    <VectorCharacter
-                      char={chainInfo.chainName[0]}
-                      height={15}
-                      color="white"
-                    />
-                  )}
+                  <BrowserIcon />
                 </View>
-                <Text
-                  style={style.flatten(
-                    ["h5", "color-text-black-very-very-low"],
-                    [selected && "color-text-black-medium"]
-                  )}
-                >
-                  {chainInfo.chainName}
+                <Text style={style.flatten(['h5', 'color-text-black-medium'])}>
+                  {'Browser'}
                 </Text>
               </RectButton>
-            );
-          })}
-          <RectButton
-            key={"browser"}
-            onPress={() => {
-              navigation.navigate("Browser");
-            }}
-            style={style.flatten([
-              "flex-row",
-              "height-84",
-              "items-center",
-              "padding-x-20",
-              "border-width-top-1",
-            ])}
-            activeOpacity={1}
-            underlayColor={style.get("color-drawer-rect-button-underlay").color}
-          >
-            <View
-              style={style.flatten([
-                "width-32",
-                "height-32",
-                "border-radius-64",
-                "items-center",
-                "justify-center",
-                "overflow-hidden",
-                "margin-right-16",
-                "background-color-profile-green",
-              ])}
-            >
-              <BrowserIcon />
-            </View>
-            <Text style={style.flatten(["h5", "color-text-black-medium"])}>
-              {"Browser"}
-            </Text>
-          </RectButton>
+            </>
+          </View>
         </View>
       </DrawerContentScrollView>
     );
