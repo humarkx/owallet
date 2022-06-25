268eb6a6a013a902d207b360fadded5332af796e
sonlha <lhas1307@gmail.com>
2022-06-25 11:33:35 +0700

    style and browser store

--- packages/mobile/src/stores/root.tsx
+++ packages/mobile/src/stores/root.tsx
@@ -11,28 +11,28 @@ import {
   LedgerInitStore,
   IBCCurrencyRegsitrar,
   PermissionStore,
-} from "@owallet/stores";
-import { AsyncKVStore } from "../common";
-import { APP_PORT } from "@owallet/router";
-import { ChainInfoWithEmbed } from "@owallet/background";
-import { RNEnv, RNRouterUI, RNMessageRequesterInternal } from "../router";
-import { ChainStore } from "./chain";
-import { DeepLinkStore } from "./deeplink";
-import EventEmitter from "eventemitter3";
-import { OWallet } from "@owallet/provider";
-import { KeychainStore } from "./keychain";
-import { WalletConnectStore } from "./wallet-connect";
-import { FeeType } from "@owallet/hooks";
+} from '@owallet/stores';
+import { AsyncKVStore } from '../common';
+import { APP_PORT } from '@owallet/router';
+import { ChainInfoWithEmbed } from '@owallet/background';
+import { RNEnv, RNRouterUI, RNMessageRequesterInternal } from '../router';
+import { ChainStore } from './chain';
+import { DeepLinkStore, BrowserStore } from './browser';
+import EventEmitter from 'eventemitter3';
+import { OWallet } from '@owallet/provider';
+import { KeychainStore } from './keychain';
+import { WalletConnectStore } from './wallet-connect';
+import { FeeType } from '@owallet/hooks';
 import {
   AmplitudeApiKey,
   EmbedChainInfos,
   UIConfigStore,
   FiatCurrencies,
-} from "@owallet/common";
-import { AnalyticsStore, NoopAnalyticsClient } from "@owallet/analytics";
-import { Amplitude } from "@amplitude/react-native";
-import { ChainIdHelper } from "@owallet/cosmos";
-import { FiatCurrency } from "@owallet/types";
+} from '@owallet/common';
+import { AnalyticsStore, NoopAnalyticsClient } from '@owallet/analytics';
+import { Amplitude } from '@amplitude/react-native';
+import { ChainIdHelper } from '@owallet/cosmos';
+import { FiatCurrency } from '@owallet/types';
 
 export class RootStore {
   public readonly uiConfigStore: UIConfigStore;
@@ -60,7 +60,7 @@ export class RootStore {
       chainName?: string;
       toChainId?: string;
       toChainName?: string;
-      registerType?: "seed" | "google" | "apple" | "ledger" | "qr";
+      registerType?: 'seed' | 'google' | 'apple' | 'ledger' | 'qr';
       feeType?: FeeType | undefined;
       isIbc?: boolean;
       validatorName?: string;
@@ -69,21 +69,22 @@ export class RootStore {
       proposalTitle?: string;
     },
     {
-      registerType?: "seed" | "google" | "ledger" | "qr" | "apple";
-      accountType?: "mnemonic" | "privateKey" | "ledger";
+      registerType?: 'seed' | 'google' | 'ledger' | 'qr' | 'apple';
+      accountType?: 'mnemonic' | 'privateKey' | 'ledger';
       currency?: string;
       language?: string;
     }
   >;
 
   public readonly deepLinkUriStore: DeepLinkStore;
+  public readonly browserStore: BrowserStore;
 
   constructor() {
     const router = new RNRouterUI(RNEnv.produceEnv);
 
     const eventEmitter = new EventEmitter();
 
-    this.uiConfigStore = new UIConfigStore(new AsyncKVStore("store_ui_config"));
+    this.uiConfigStore = new UIConfigStore(new AsyncKVStore('store_ui_config'));
 
     // Order is important.
     this.interactionStore = new InteractionStore(
@@ -103,7 +104,7 @@ export class RootStore {
     this.chainStore = new ChainStore(
       EmbedChainInfos,
       new RNMessageRequesterInternal(),
-      new AsyncKVStore("store_chains")
+      new AsyncKVStore('store_chains')
     );
 
     this.keyRingStore = new KeyRingStore(
@@ -112,7 +113,7 @@ export class RootStore {
           eventEmitter.emit(type);
         },
       },
-      "pbkdf2",
+      'pbkdf2',
       this.chainStore,
       new RNMessageRequesterInternal(),
       this.interactionStore
@@ -123,11 +124,11 @@ export class RootStore {
       // In the case of storage where the prefix key is "store_queries" or "store_queries_fix", we should not use it because it is already corrupted in some users.
       // https://github.com/chainapsis/owallet-wallet/issues/275
       // https://github.com/chainapsis/owallet-wallet/issues/278
-      new AsyncKVStore("store_queries_fix2"),
+      new AsyncKVStore('store_queries_fix2'),
       this.chainStore,
       async () => {
         // TOOD: Set version for OWallet API
-        return new OWallet("", "core", new RNMessageRequesterInternal());
+        return new OWallet('', 'core', new RNMessageRequesterInternal());
       },
       QueriesWithCosmosAndSecretAndCosmwasmAndEvm
     );
@@ -151,11 +152,11 @@ export class RootStore {
           autoInit: true,
           getOWallet: async () => {
             // TOOD: Set version for OWallet API
-            return new OWallet("", "core", new RNMessageRequesterInternal());
+            return new OWallet('', 'core', new RNMessageRequesterInternal());
           },
         },
         chainOpts: this.chainStore.chainInfos.map((chainInfo) => {
-          if (chainInfo.chainId.startsWith("osmosis")) {
+          if (chainInfo.chainId.startsWith('osmosis')) {
             return {
               chainId: chainInfo.chainId,
               msgOpts: {
@@ -172,14 +173,14 @@ export class RootStore {
     );
 
     this.priceStore = new CoinGeckoPriceStore(
-      new AsyncKVStore("store_prices"),
+      new AsyncKVStore('store_prices'),
       FiatCurrencies.reduce<{
         [vsCurrency: string]: FiatCurrency;
       }>((obj, fiat) => {
         obj[fiat.currency] = fiat;
         return obj;
       }, {}),
-      "usd"
+      'usd'
     );
 
     this.tokensStore = new TokensStore(
@@ -194,7 +195,7 @@ export class RootStore {
     );
 
     this.ibcCurrencyRegistrar = new IBCCurrencyRegsitrar<ChainInfoWithEmbed>(
-      new AsyncKVStore("store_test_ibc_currency_registrar"),
+      new AsyncKVStore('store_test_ibc_currency_registrar'),
       24 * 3600 * 1000,
       this.chainStore,
       this.accountStore,
@@ -205,12 +206,12 @@ export class RootStore {
     router.listen(APP_PORT);
 
     this.keychainStore = new KeychainStore(
-      new AsyncKVStore("store_keychain"),
+      new AsyncKVStore('store_keychain'),
       this.keyRingStore
     );
 
     this.walletConnectStore = new WalletConnectStore(
-      new AsyncKVStore("store_wallet_connect"),
+      new AsyncKVStore('store_wallet_connect'),
       {
         addEventListener: (type: string, fn: () => void) => {
           eventEmitter.addListener(type, fn);
@@ -263,6 +264,7 @@ export class RootStore {
       }
     );
     this.deepLinkUriStore = new DeepLinkStore();
+    this.browserStore = new BrowserStore();
   }
 }
 
