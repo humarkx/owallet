fe71aa53b04008092247dfe0800c64a71aea2091
sonlha <lhas1307@gmail.com>
2022-07-05 17:49:04 +0700

    add currency type

--- packages/mobile/src/modals/sign/index.tsx
+++ packages/mobile/src/modals/sign/index.tsx
@@ -270,7 +270,7 @@ export const SignModal: FunctionComponent<{
               try {
                 if (signDocHelper.signDocWrapper) {
                   //
-                  signInteractionStore.reject();
+                  signInteractionStore.rejectAll();
                 }
               } catch (error) {
                 console.log(error);
