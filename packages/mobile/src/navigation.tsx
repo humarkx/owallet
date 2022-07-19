c38edc0ab124db20047151df40c6df791909ed8f
sonlha <lhas1307@gmail.com>
2022-07-19 13:47:51 +0700

    codepush download screen #WIP

--- packages/mobile/src/navigation.tsx
+++ packages/mobile/src/navigation.tsx
@@ -113,6 +113,7 @@ import { colors, spacing, typography } from './themes';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Hash } from '@owallet/crypto';
 import { useRoute } from '@react-navigation/core';
+import CodePush from 'react-native-code-push';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -1042,6 +1043,42 @@ export const AppNavigation: FunctionComponent = observer(() => {
       Linking.removeEventListener('url', handleDeepLink);
     };
   }, []);
+
+  // useEffect(() => {
+  //   CodePush.sync(
+  //     {
+  //       updateDialog: {
+  //         appendReleaseDescription: true,
+  //         title: 'Update available'
+  //       },
+  //       installMode: CodePush.InstallMode.IMMEDIATE
+  //     },
+  //     status => {
+  //       switch (status) {
+  //         case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+  //           console.log('DOWNLOADING_PACKAGE');
+  //           // Show "downloading" modal
+  //           // modal.open();
+  //           break;
+  //         case CodePush.SyncStatus.INSTALLING_UPDATE:
+  //           console.log('INSTALLING_UPDATE');
+  //           // show installing
+  //           // setState({ showInstalling: true });
+  //           break;
+  //         case CodePush.SyncStatus.UPDATE_INSTALLED:
+  //           console.log('UPDATE_INSTALLED');
+  //           // Hide loading modal
+  //           // modal.close();
+  //           // setState({ showDownloadingModal: false });
+  //           break;
+  //       }
+  //     },
+  //     ({ receivedBytes, totalBytes }) => {
+  //       /* Update download modal progress */
+  //       // setState({ downloadProgress: (receivedBytes / totalBytes) * 100 });
+  //     }
+  //   );
+  // }, []);
 
   return (
     <PageScrollPositionProvider>
